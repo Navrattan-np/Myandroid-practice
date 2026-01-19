cb755e8bf154d80cb9840363eb607efc0f3fd036
Nitin-pannu <ntnpannu@gmail.com>
2026-01-19 19:41:53 +0530

    basic of app

---
+++ ShoppingListapp/app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -0,0 +1,41 @@
+package com.example.shoppinglistapp
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+data class ShoppingItem(
+    val id: Int,
+    var name: String,
+    var qty: Int,
+    var isEditing: Boolean = false
+)
+
+@Composable
+fun ShoppingListApp() {
+    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { showDialog = true },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Add Item")
+        }
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(16.dp)
+        ) {
+            // We will add the list items here tomorrow!
+        }
+    }
+}
