2a86380236c7f81e72e019a0579517944c2a3e99
Nitin-pannu <ntnpannu@gmail.com>
2026-02-19 20:55:41 +0530

    add item view

--- MyWishListApp/app/src/main/java/com/example/mywishlistapp/WishViewModel.kt
+++ MyWishListApp/app/src/main/java/com/example/mywishlistapp/WishViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.mywishlistapp
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class WishViewModel: ViewModel() {
+
+    var wishTitleState by mutableStateOf("")
+    var wishDescriptionState by mutableStateOf("")
+
+    fun onWishTitlechanged(newString:String){
+        wishTitleState=newString
+    }
+    fun onWishDescriptionState(newString:String){
+        wishDescriptionState=newString
+    }
 
 }
