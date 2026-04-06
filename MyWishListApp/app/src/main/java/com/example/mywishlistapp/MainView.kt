64d8ea08aca8fe2b3b88b4be8e8a8535da8c19fe
Navrattan <ntnpannu@gmail.com>
2026-04-06 20:26:43 +0530

    Refactor HomeView and WishItem components

--- MyWishListApp/app/src/main/java/com/example/mywishlistapp/MainView.kt
+++ MyWishListApp/app/src/main/java/com/example/mywishlistapp/MainView.kt
@@ -9,69 +9,74 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.mywishlistapp.data.DummyWish
 import com.example.mywishlistapp.data.Wish
 
 @Composable
-fun HomeView(
-    viewModel:WishViewModel,
-    navController:NavHostController
-){
-   val context= LocalContext.current
+fun HomeView(navController: NavController){
+    val context=LocalContext.current
     Scaffold(
-         topBar={ AppBarView(title="WishList",
-                             onBackNavClicked={
-                                Toast.makeText(context,"button clicked",Toast.LENGTH_LONG).show()
-
-                             }
-                  )
-              },
-        floatingActionButton = { FloatingActionButton(
-                                        onClick = { Toast.makeText(context,"Add button clicked",Toast.LENGTH_LONG).show()
-                                            navController.navigate(Screen.AddScreen.route)
-                                                  },
-                                        modifier=Modifier.padding(all=20.dp),
-                                        contentColor= Color.White,
-                                        backgroundColor= Color.Black
-                                  ) {
-                                      Icon(imageVector = Icons.Default.Add, contentDescription = null)
-        }}
-    ){
+        topBar={AppBarView(title="WishList",
+            onBackNavClicked={ Toast.makeText(context,"Button Clicked",Toast.LENGTH_LONG).show() }
+        )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick={Toast.makeText(context,"FAB clicked",Toast.LENGTH_LONG).show()
+                    navController.navigate(Screen.AddScreen.route)
+                },
+                contentColor= Color.White,
+                modifier=Modifier.padding(all=20.dp),
+                containerColor=Color.Black
+            ){
+                Icon(imageVector = Icons.Default.Add, contentDescription =null )
+            }
+        }
+    ) {
         LazyColumn(modifier= Modifier
             .fillMaxSize()
             .padding(it)){
-                items(DummyWish.wishList){wish->
-                    wishItem(wish,{})
-                }
+            items(DummyWish.wishList){ wish->
+                WishItem(wish = wish)
+            }
         }
     }
 }
 
 @Composable
-fun wishItem(wish: Wish,onClick: ()->Unit){
+fun WishItem(wish: Wish, onClick: ()->Unit={}){
     Card(
-            modifier=Modifier.fillMaxWidth()
-                .padding(top=8.dp,start=8.dp,end=8.dp)
-                .clickable { onClick() },
-            elevation=10.dp,
-            backgroundColor = Color.White
-        ){
-            Column(modifier=Modifier.padding(16.dp)){
-                 Text(wish.title,fontWeight= FontWeight.ExtraBold)
-                 Text(wish.description)
-            }
+        modifier= Modifier
+            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+            .fillMaxWidth()
+            .clickable { onClick() },
+        elevation = 10.dp,
+        backgroundColor=Color.White
+    ) {
+        Column(modifier=Modifier.padding(16.dp)){
+            Text(wish.title,fontWeight= FontWeight.ExtraBold)
+            Text(wish.description)
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainViewPreview(){
+    HomeView(rememberNavController())
 }
